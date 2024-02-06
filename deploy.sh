4d5e93e5e44f84f3befc8f64b0789d1edfdd3ea3
GallonShih <gallonshih0526@gmail.com>
2024-02-06 21:53:40 +0800

    fix: use temp to make docker-compose.yaml

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 IMAGE_INFO=$1
-TEMPLATE_FILE=docker-compose.yaml
+TEMPLATE_FILE=docker-compose.template.yaml
 OUTPUT_FILE=docker-compose.yaml
 
 sed "s|image: gallonshih/cicd_example.*|image: $IMAGE_INFO|g" $TEMPLATE_FILE > $OUTPUT_FILE
 
+echo "Updated docker-compose.yaml:"
 cat $OUTPUT_FILE
